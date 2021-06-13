ff7856f037c41a411e59a9c108685565c3859748
zhongpeng <859247268@qq.com>
2021-06-13 14:27:47 +0800

    feat: 登陆模块表单布局

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,6 +1,55 @@
 <template>
   <div class="login-box">
-    <div class="form-box"></div>
+    <div class="form-box">
+      <div class="title-box">
+        <img src="../../assets/login-logo.png" alt="" />
+        <span class="title">黑马面面</span>
+        <i></i>
+        <span class="sub-title">用户登录</span>
+      </div>
+      <div class="login-form">
+        <el-form ref="form" :model="form">
+          <el-form-item>
+            <el-input
+              placeholder="请输入手机号"
+              v-model="form.name"
+              prefix-icon="el-icon-user"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              placeholder="请输入密码"
+              v-model="form.name"
+              prefix-icon="el-icon-lock"
+              show-password
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-row>
+              <el-col :span="18">
+                <el-input
+                  placeholder="请输入验证码"
+                  v-model="form.name"
+                  prefix-icon="el-icon-key"
+                ></el-input>
+              </el-col>
+              <el-col :span="6">
+                <img class="capatcha" src="../../assets/captcha.jpg" alt="" />
+              </el-col>
+            </el-row>
+          </el-form-item>
+          <el-form-item>
+            <el-checkbox v-model="form.checked">
+              我已阅读并同意<el-link type="primary">用户协议</el-link>和<el-link type="primary">隐私条款</el-link>
+            </el-checkbox>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary">登录</el-button>
+            <el-button class="register-button" type="success">取消</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
     <img src="../../assets/login_banner_ele.png" alt="" class="login-pic" />
   </div>
 </template>
@@ -8,20 +57,90 @@
 <script>
 export default {
   name: "login",
+  data() {
+    return {
+      form: {
+        name: "",
+        checked: false,
+      },
+    };
+  },
 };
 </script>
 
-<style>
+<style lang="less">
 .login-box {
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    background: linear-gradient(225deg, rgba(20, 147, 250, 1), rgba(1, 198, 250, 1));
-    height: 100%;
-}
-.login-box .form-box {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background: linear-gradient(
+    225deg,
+    rgba(20, 147, 250, 1),
+    rgba(1, 198, 250, 1)
+  );
+  height: 100%;
+
+  .form-box {
     width: 478px;
     height: 550px;
     background-color: #f5f5f5;
+    padding: 44px;
+    box-sizing: border-box;
+
+    .title-box {
+      display: flex;
+      align-items: center;
+
+      img {
+        width: 22px;
+        height: 17px;
+        margin-right: 16px;
+      }
+
+      .title {
+        font-size: 24px;
+        margin-right: 14px;
+      }
+
+      i {
+        width: 1px;
+        height: 28px;
+        background-color: #c7c7c7;
+        margin-right: 12px;
+      }
+
+      .sub-title {
+        font-size: 22px;
+      }
+    }
+
+    .login-form {
+      margin-top: 27px;
+
+      .capatcha {
+        height: 40px;
+        width: 100%;
+      }
+
+      .el-checkbox {
+        display: flex;
+        align-items: center;
+
+        .el-checkbox__label {
+          display: flex;
+          align-items: center;
+        }
+      }
+
+      .el-button {
+        width: 100%;
+        margin: 0;
+      }
+
+      .register-button {
+        margin-top: 26px;
+      }
+    }
+  }
 }
 </style>
