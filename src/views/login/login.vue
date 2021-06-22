1ad1f8280dd7b4c3416d668bc12e5f65bedd0649
zhongpeng <859247268@qq.com>
2021-06-22 10:17:01 +0800

    feat: 登陆模块表单验证完成

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -8,28 +8,28 @@
         <span class="sub-title">用户登录</span>
       </div>
       <div class="login-form">
-        <el-form ref="form" :model="form">
-          <el-form-item>
+        <el-form ref="form" :model="form" :rules="rules">
+          <el-form-item prop="phone">
             <el-input
               placeholder="请输入手机号"
-              v-model="form.name"
+              v-model="form.phone"
               prefix-icon="el-icon-user"
             ></el-input>
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop="password">
             <el-input
               placeholder="请输入密码"
-              v-model="form.name"
+              v-model="form.password"
               prefix-icon="el-icon-lock"
               show-password
             ></el-input>
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop="captcha">
             <el-row>
               <el-col :span="18">
                 <el-input
                   placeholder="请输入验证码"
-                  v-model="form.name"
+                  v-model="form.captcha"
                   prefix-icon="el-icon-key"
                 ></el-input>
               </el-col>
@@ -40,11 +40,14 @@
           </el-form-item>
           <el-form-item>
             <el-checkbox v-model="form.checked">
-              我已阅读并同意<el-link type="primary">用户协议</el-link>和<el-link type="primary">隐私条款</el-link>
+              我已阅读并同意<el-link type="primary">用户协议</el-link>和<el-link
+                type="primary"
+                >隐私条款</el-link
+              >
             </el-checkbox>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary">登录</el-button>
+            <el-button type="primary" @click="submitForm">登录</el-button>
             <el-button class="register-button" type="success">取消</el-button>
           </el-form-item>
         </el-form>
@@ -58,12 +61,79 @@
 export default {
   name: "login",
   data() {
+    // 自定义校验规则的函数：
+    var checkPhone = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("手机号不能为空"));
+      } else {
+        // 判断手机号格式
+        const reg = /0?(13|14|15|18)[0-9]{9}/;
+        if (reg.test(value) == true) {
+          callback();
+        } else {
+          callback(new Error("老铁，你的手机号写错了噢"));
+        }
+      }
+    };
+
     return {
+      // 表单数据：
       form: {
-        name: "",
+        phone: "",
+        password: "",
+        captcha: "",
         checked: false,
       },
+      // 定义校验规则：
+      rules: {
+        phone: [{ required: true, validator: checkPhone, trigger: "blur" }],
+        password: [
+          {
+            required: true,
+            message: "密码不能为空",
+            trigger: "change",
+          },
+          {
+            min: 6,
+            max: 18,
+            message: "密码长度为6到18",
+            trigger: "change",
+          },
+        ],
+        captcha: [
+          {
+            required: true,
+            message: "验证码不能为空",
+            trigger: "change",
+          },
+          {
+            min: 4,
+            max: 4,
+            message: "验证码长度为4",
+            trigger: "change",
+          },
+        ],
+      },
     };
+  },
+  methods: {
+    submitForm() {
+      if (this.form.checked == false) {
+        this.$message.warning("请勾选用户协议！");
+      } else {
+        this.$refs.form.validate((valid) => {
+          if (valid) {
+            // 验证成功
+            this.$message.success("恭喜你，成功啦");
+          } else {
+            // 验证失败
+            this.$message.error("很遗憾，输入错误");
+
+            return false;
+          }
+        });
+      }
+    },
   },
 };
 </script>
