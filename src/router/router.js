b5412e3c9caefd5c62649dae703f73cec7df0c53
zhongpeng <859247268@qq.com>
2021-06-25 17:03:08 +0800

    feat: 首页模块整体布局

--- src/router/router.js
+++ src/router/router.js
@@ -1,16 +1,21 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import login from "../views/login/login.vue";
+import index from "../views/index/index.vue";
 
 // 安装vue-router
 Vue.use(VueRouter);
 
 // 定义路由规则：
 const routes = [
-    {
-        path: "/login",
-        component: login
-    }
+  {
+    path: "/login",
+    component: login,
+  },
+  {
+    path: "/index",
+    component: index,
+  },
 ];
 
 // 创建路由实例
---
+++ src/views/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-container class="index-container">
+    <el-header class="my-header">Header</el-header>
+    <el-container>
+      <el-aside width="200px" class="my-aside">Aside</el-aside>
+      <el-main class="my-main">Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "index",
+};
+</script>
+
+<style lang="less">
+.index-container {
+  height: 100%;
+
+  .my-header {
+    background-color: skyblue;
+  }
+  .my-aside {
+    background-color: green;
+  }
+  .my-main {
+    background-color: purple;
+  }
+}
+</style>
