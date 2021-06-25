b170572727bd5950d6a58219961ea0831775ebf0
zhongpeng <859247268@qq.com>
2021-06-25 18:04:56 +0800

    feat: 左侧导航整合

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -13,7 +13,35 @@
       </div>
     </el-header>
     <el-container>
-      <el-aside width="200px" class="my-aside">Aside</el-aside>
+      <el-aside width="200px" class="my-aside">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+        >
+          <el-menu-item index="1">
+            <i class="el-icon-pie-chart"></i>
+            <span slot="title">数据概览</span>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <i class="el-icon-user"></i>
+            <span slot="title">用户列表</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-edit-outline"></i>
+            <span slot="title">题库列表</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-office-building"></i>
+            <span slot="title">企业列表</span>
+          </el-menu-item>
+          <el-menu-item index="5">
+            <i class="el-icon-notebook-2"></i>
+            <span slot="title">学科列表</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
       <el-main class="my-main">Main</el-main>
     </el-container>
   </el-container>
@@ -30,7 +58,7 @@ export default {
   height: 100%;
 
   .my-header {
-    background-color: skyblue;
+    // background-color: skyblue;
     display: flex;
     justify-content: space-between;
     .left {
@@ -66,7 +94,7 @@ export default {
     }
   }
   .my-aside {
-    background-color: green;
+    // background-color: green;
   }
   .my-main {
     background-color: purple;
